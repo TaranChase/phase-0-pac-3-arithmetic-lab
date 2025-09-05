554f7ddea4f2b0363bc6de5dcb71fd0ae67b9927
TaranChase <chase.taran@gmail.com>
2025-09-05 19:13:44 +0300

    Attempting math using math operators and parsing strings as numbers.

---
+++ index.js
@@ -0,0 +1,59 @@
+function add(a, b) {
+    return a+b;
+
+}
+
+console.log(add(700, 81));
+
+
+function subtract(a, b) {
+    return a-b;
+
+}
+
+console.log(subtract(4,1));
+
+
+function multiply(a,b) {
+    return a*b;
+}
+
+console.log(multiply(76323));
+
+
+function divide(a,b) {
+    return a/b;
+
+}
+
+console.log(divide(1, 2.4955223880597015));
+
+
+function increment(n) {
+    return n += 1;
+}
+
+console.log(increment(5));
+
+
+function decrement(n) {
+    return n -= 1;
+
+}
+
+console.log(decrement(4));
+
+
+function makeInt(n) {
+    return parseInt(n, "10");
+
+}
+
+console.log(makeInt(5));
+
+
+function preserveDecimal(n) {
+    return parseFloat(n, "12");
+}
+
+console.log(parseFloat(6));
